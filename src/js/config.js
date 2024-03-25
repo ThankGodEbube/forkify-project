549f13c58f1fd76a9f810bc2d347b2ccc5978f9a
ThankGodEbube <oluebubethankgod7@gmail.com>
2024-03-25 15:17:09 +0100

    cleaned config

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = "9882ce2a-15c4-4824-8681-a84372670e2d";
+export const KEY = "2e0de6e2-b3a1-4aed-b48f-80562490fe7f";
 export const MODAL_CLOSE_SEC = 2.5;
